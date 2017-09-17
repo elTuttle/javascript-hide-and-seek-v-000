1292bcab00e5c25b98c064259b5783fbd31980ed
John Tuttle <thejwt@gmail.com>
2017-09-17 07:00:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function increaseRankBy(n){
   const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
 
   for (let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = (lis[i] + 1).toString()
+    console.log(lis[i])
   }
 }
