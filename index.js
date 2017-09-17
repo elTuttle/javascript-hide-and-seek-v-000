cec2a76da0dee67fa4a1bd71fdd172441e64a56d
John Tuttle <thejwt@gmail.com>
2017-09-17 06:53:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function getFirstSelector(selector){
   const selectorValue = document.querySelector(selector)
   return selectorValue
 }
+
+function nestedTarget(){
+  const div = document.querySelectorAll("div.nested, div.target")
+  return div
+}
