5952dbee64894bb4bdf0202e529a10ce95715578
John Tuttle <thejwt@gmail.com>
2017-09-17 06:58:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ function nestedTarget(){
 }
 
 function deepestChild(){
-  
+
 }
 
 function increaseRankBy(n){
   const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
- 
+
   for (let i = 0; i < lis.length; i++) {
     lis[i].innerHTML = (i + 1).toString()
   }
