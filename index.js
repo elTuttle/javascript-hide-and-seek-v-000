1ba08c0364e0642c1987f0ed024b1da439341be0
John Tuttle <thejwt@gmail.com>
2017-09-17 08:36:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector){
+  const selectorValue = document.querySelector(selector)
+  return selectorValue
+}
+
+function nestedTarget(){
+  const div = document.querySelector("div.nested, div.target")
+  return div
+}
+
+function deepestChild(){
+  const div = document.querySelector('#grand-node div div div div')
+  return div
+}
+
+function increaseRankBy(n){
+  const rankedLists = document.querySelectorAll('.ranked-list')
+  for (let i = 0; i < rankedLists.length; i++) {
+    rankedLists[i].innerHTML = (parseInt(rankedLists[i].innerHTML + n))
+
+  }
+}
