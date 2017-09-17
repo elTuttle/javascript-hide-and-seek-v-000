0bcd746437f8d83336ce68351d99319189635b54
John Tuttle <thejwt@gmail.com>
2017-09-17 08:31:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,13 @@ function deepestChild(){
 }
 
 function increaseRankBy(n){
-  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  const rankedLists = document.querySelectorAll('.ranked-list')
+  const firstList = rankedLists[0]
+  const secondList = rankedLists[1]
 
-  for (let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = (i + 1 + n).toString()
+  let children = firstList.children
+
+  for (let i = 0; i < children.length; i++) {
+    children[i].innerHTML = (parseInt(children[i].innerHTML + n))
   }
 }
