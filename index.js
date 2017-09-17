1457ef8faca8ee7611dbdd51ab723c838a6bd7d0
John Tuttle <thejwt@gmail.com>
2017-09-17 06:58:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ function nestedTarget(){
   const div = document.querySelector("div.nested, div.target")
   return div
 }
+
+function deepestChild(){
+  
+}
+
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+ 
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (i + 1).toString()
+  }
+}
